2c51670754d39c3769816179032e4f0d8dfca4f3
camilo <juancamilo.mosqueram@hotmail.com>
2021-10-26 16:06:17 -0500

    files added

--- src/com/java/data/Calculator.java
+++ src/com/java/data/Calculator.java
@@ -1,6 +1,14 @@
 package com.java.data;
 
 public class Calculator {
+
+    public float sum (float num1, float num2){
+        return num1+num2;
+    }
+
+    public float subtract (float num1, float num2){
+        return num1+num2;
+    }
     
 
 }
