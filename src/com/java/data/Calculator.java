8e40b6bf91fa8eccf03e84c0cd5c53793ccf534a
camilo <juancamilo.mosqueram@hotmail.com>
2021-10-28 18:17:19 -0500

    my Calculator

--- src/com/java/data/Calculator.java
+++ src/com/java/data/Calculator.java
@@ -1,22 +1,27 @@
 package com.java.data;
 
 public class Calculator {
+    public static void main(String[] args) {
+        System.out.println(sum(10,4));
+        System.out.println(subtract(4,5));
+        System.out.println(multiplication(2,6));
+        System.out.println(division(6,4));
+    }
 
 
-    public float sum (float num1, float num2){
+    public static float sum (float num1, float num2){
         return num1+num2;
     }
 
-    public float subtract (float num1, float num2){
+    public static float subtract (float num1, float num2){
         return num1+num2;
     }
-    
 
-    public float multiplication (float num1, float num2){
+    public static float multiplication (float num1, float num2){
         return num1*num2;
     }
 
-    public float division (float num1, float num2){
+    public static float division (float num1, float num2){
         return num1/num2;
     }
 
